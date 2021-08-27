f5372da5d7a143a9230764fd5ad5355cc6794413
dandy <danielsbarbosa27@gmail.com>
2021-08-27 17:16:44 -0300

    ajustes - data

--- clean_architecture/lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ clean_architecture/lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -1,9 +1,9 @@
 import 'package:dartz/dartz.dart';
 
-import '../../../core/error/failures.dart';
-import '../../../core/usecase/usecase.dart';
-import 'entities/number_trivia.dart';
-import 'repositories/number_trivia_repository.dart';
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecase/usecase.dart';
+import '../entities/number_trivia.dart';
+import '../repositories/number_trivia_repository.dart';
 
 class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
   late final NumberTriviaRepository repository;
--- clean_architecture/test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ clean_architecture/test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -1,6 +1,6 @@
 import 'package:clean_architecture/core/usecase/usecase.dart';
 import 'package:clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:clean_architecture/features/number_trivia/domain/get_random_number_trivia.dart';
+import 'package:clean_architecture/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
